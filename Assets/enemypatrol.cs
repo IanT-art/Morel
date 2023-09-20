d6ca936ca5fdba123da056347c26be6e9475e905
Yukarn <145293277+Yukarn@users.noreply.github.com>
2023-09-20 12:29:06 -0400

    flip when enemy reaches other side

--- Assets/enemypatrol.cs
+++ Assets/enemypatrol.cs
@@ -34,13 +34,22 @@ public class enemypatrol : MonoBehaviour
 
         if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
         {
+            flip();
             currentPoint = pointA.transform;
         }
 
         if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
         {
+            flip();
             currentPoint = pointB.transform;
         }
+    }
+
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
     }
 
     private void OnDrawGizmos()
